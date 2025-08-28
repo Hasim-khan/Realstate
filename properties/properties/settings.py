96463d79f1384d1afffdfc314c561c002eba9422
Hasim-khan <hasimkhan4440@gmail.com>
2025-08-28 12:34:33 +0530

    third commit for changes vnv and requirements file

--- properties/properties/settings.py
+++ properties/properties/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wcd5uklhyabj-=0uf=^_s9+&$cqdhs7y$*_7#s^wgs%#2d*3p8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app1',
 ]
 
 MIDDLEWARE = [
