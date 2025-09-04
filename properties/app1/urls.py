c5a752d22a978c512905feab871d4767f6aec154
Hasim-khan <hasimkhan4440@gmail.com>
2025-09-04 19:43:05 +0530

    add pages abou contact blogs

--- properties/app1/urls.py
+++ properties/app1/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),   # Example
+    path('', views.index, name='home'), 
+    path('home/', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
+        # Example
 ]
--- properties/app1/views.py
+++ properties/app1/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about-us.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def blog(request):
+    return render(request, 'blog.html')
